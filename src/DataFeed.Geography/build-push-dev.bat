43d4831863b7bc67658e907279a2dc099781574b
yangliangbin <yangliangbin@hyihk.com>
2019-11-09 18:36:36 +0800

    -

--- src/DataFeed.Geography/build-push-dev.bat
+++ src/DataFeed.Geography/build-push-dev.bat
@@ -3,7 +3,7 @@ for /f "tokens=1,2,3 delims=:." %%a in ("%time%") do @set T=%%a%%b%%c
 set T=%T: =0%
 set currenttime=%D%%T%
 set imagetag=datafeed-geography:%currenttime%
-set dockerserver=111.230.252.214:5001
+set dockerserver=111.230.252.214:5000
 set projectname=dev
 set image=%dockerserver%/%projectname%/%imagetag%
 docker build --rm -t %imagetag% -f Dockerfile ..
