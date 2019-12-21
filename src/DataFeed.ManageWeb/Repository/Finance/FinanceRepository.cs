0a7518eff1cfb8efa10bd1c0c97b6ded8c75db2e
git <gx_ylb@126.com>
2019-12-21 20:54:53 +0800

    --

--- src/DataFeed.ManageWeb/Repository/Finance/FinanceRepository.cs
+++ src/DataFeed.ManageWeb/Repository/Finance/FinanceRepository.cs
@@ -6,10 +6,6 @@ using System.Text;
 using System.Data;
 using Dapper;
 using DataFeed.Framework.Repository;
-using DataFeed.ManageWeb.Contract.Response;
-using FastNet.Framework.Dapper;
-using DataFeed.ManageWeb.Contract.Request;
-using DataFeed.Framework.Model;
 
 namespace DataFeed.ManageWeb.Repository
 {
